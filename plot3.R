d57ba57762d2325e17767960e4fb03d044044c0b
laicen <laicenteno@gmail.com>
2015-04-12 00:15:48 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,5 +6,5 @@ plot(epc$Date_Time, epc$Sub_metering_1, ylab="Energy sub metering", xlab="", typ
 lines(epc$Date_Time, epc$Sub_metering_1)
 lines(epc$Date_Time, epc$Sub_metering_2, col="red")
 lines(epc$Date_Time, epc$Sub_metering_3, col="blue")
-legend("topright", pch="__________", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lty=1, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
